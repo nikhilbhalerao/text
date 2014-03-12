4dc9f0005a9e4bbbb66c04a5ea89f300cdc6aa15
1649 <nikhil@1649-0218-0222.(none)>
2014-03-12 12:26:12 +0530

    fifth commit

---
+++ task_3/php/regs.php
@@ -0,0 +1,17 @@
+<h3>WELCOME TO REGESTRATION PAGE</h3>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nikhil
+ * Date: 11/3/14
+ * Time: 8:38 PM
+ */
+$fname = $_POST["fname"];
+$lname = $_POST["lname"];
+$add = $_POST["add"];
+$ph = $_POST["ph"];
+echo 'welcome '. $fname . "<br>";
+echo ''. $lname . "<br>";
+echo ''. $add . "<br>";
+echo ''. $ph . "<br>";
+?>
